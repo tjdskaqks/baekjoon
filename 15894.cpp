85a6bd90222d4477ebe6e8b25a95de7331f0dc27
SeongHyun Jeon <45706636+tjdskaqks@users.noreply.github.com>
2019-01-04 16:34:22 +0900

    수학은 체육과목입니다

---
+++ 15894.cpp
@@ -0,0 +1,17 @@
+/*
+	[15894] 수학은 체육과목입니다
+	https://www.acmicpc.net/problem/15894
+	https://github.com/tjdskaqks
+	http://jcoder1.tistory.com/
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned int  n;
+  
+	cin >> n;
+	cout << n * 4 << endl;
+	return 0;
+}
