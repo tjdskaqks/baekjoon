f95c34bd5e425daaf4dd32a0169625acd9cce0ee
SeongHyun Jeon <45706636+tjdskaqks@users.noreply.github.com>
2019-01-04 16:33:37 +0900

    단어 길이 재기

---
+++ 2743.cpp
@@ -0,0 +1,16 @@
+/*
+	[2743] 단어 길이 재기
+	https://www.acmicpc.net/problem/2743
+	https://github.com/tjdskaqks
+	http://jcoder1.tistory.com/
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << s.size() << endl;
+}
