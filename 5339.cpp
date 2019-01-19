eebedc8ee3306eb85e365e34d116cfebadf2ee1e
SeongHyun Jeon <45706636+tjdskaqks@users.noreply.github.com>
2019-01-19 14:23:39 +0900

    콜센터

---
+++ 5339.cpp
@@ -0,0 +1,23 @@
+/*
+	[5339] 콜센터
+	https://www.acmicpc.net/problem/5339
+	https://github.com/tjdskaqks
+	http://jcoder1.tistory.com/
+*/
+
+#include <iostream> 
+
+int main()
+{
+	printf("     /~\\\n");
+	printf("    ( oo|\n");
+	printf("    _\\=/_\n");
+	printf("   /  _  \\\n");
+	printf("  //|/.\\|\\\\ \n");
+	printf(" ||  \\ /  ||\n");
+	printf("============\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	printf("|          |\n");
+	return 0;
+}
